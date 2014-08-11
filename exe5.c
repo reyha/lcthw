97a5ee59371752654169559181ac87944a9eb232
reyha <reyhaverma@somehow.com>
2014-08-11 19:55:03 +0530

    Initial commit

---
+++ exe5.c
@@ -0,0 +1,17 @@
+/*THE FOLLOWING CODE*/
+include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+    int areas[] = {10, 12, 13, 14, 20};
+    // WARNING: On some systems you may have to change the
+    // %ld in this code to a %u since it will use unsigned ints
+    printf("The size of an int %d", sizeof(int));
+}
+
+/re
+
+
+
+
---
+++ s.c
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if \( $1="apple" \)
+then 
+echo "hello world"
+else
+echo "bye world"
+fi
